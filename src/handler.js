e4fbab3cf19fc49e6ae8286792ad50f1d26ab033
Fatwa Aulia <fatwaaulia.fy@gmail.com>
2023-04-24 13:39:06 +0700

    fix bug finished

--- src/handler.js
+++ src/handler.js
@@ -10,7 +10,7 @@ const addBookHandler = (request, h) => {
   const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload;
 
   const id = nanoid(16);
-  const finished = (pageCount === readPage) ? 'false' : 'true';
+  const finished = (pageCount === readPage) ? 'true' : 'false';
   const insertedAt = new Date().toISOString();
   const updatedAt = insertedAt;
 
